77da19abdb5d2ccad351c8fbfd7034d110fdcbda
Emanuele Brugnara <mr.emabru@gmail.com>
2019-09-30 12:22:44 +0200

    Modulo

---
+++ fuzzy_octo_computing_machine/src/fuzzy_octo_computing_machine/Modulo.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fuzzy_octo_computing_machine;
+
+/**
+ *
+ * @author emanuele.brugnara
+ */
+public class Modulo {
+    
+    public Modulo(){}
+    
+    public double resto(double dividendo, double divisore){
+        return dividendo%divisore;
+    }
+    
+}
+
+
