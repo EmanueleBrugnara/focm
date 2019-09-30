dcfd269493cbe785782d2122f3fe3087da9dd0e9
Mohssine <mohssineelarfaoui73@gmail.com>
2019-09-30 12:21:28 +0200

    prime

--- fuzzy_octo_computing_machine/src/fuzzy_octo_computing_machine/Fuzzy_octo_computing_machine.java
+++ fuzzy_octo_computing_machine/src/fuzzy_octo_computing_machine/Fuzzy_octo_computing_machine.java
@@ -15,7 +15,6 @@ public class Fuzzy_octo_computing_machine {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
     }
     
 }
